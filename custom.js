49c408d07bd52a5c4592f1efdcfab791c0ad153c
JABASCRIPTER008 <105946792+JABASCRIPTER008@users.noreply.github.com>
2022-12-08 18:55:06 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,6 @@
+    let feed = new Instafeed({
+    get: 'ehossofficial',
+    userId: '1103665420273626',
+    accessToken: 'IGQVJXb1J0SXlYOUVKVVcwcmV0UzR0WC1GdnA2cWFkSG9vZAXhNazFvSjRjMzlOekxyR0F1Yi1hMUMwcVJoZA05TOVllTDNRcWprc0RKUnUzSTN2V0pfNU9LT1d3MTBpdTktamVWajlKb2pwXzBvUWFXRgZDZD',
+});
+    feed.run();
